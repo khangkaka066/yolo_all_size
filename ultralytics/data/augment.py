a1c9920e78fd0bdded05d0842ede1c71cd1870c3
UltralyticsAssistant <web@ultralytics.com>
2026-07-01 04:18:07 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/data/augment.py
+++ ultralytics/data/augment.py
@@ -463,9 +463,7 @@ class Mosaic(BaseMixTransform):
         assert n in {4, 9}, "grid must be equal to 4 or 9."
         super().__init__(dataset=dataset, p=p)
         self.imgsz = tuple(imgsz) if isinstance(imgsz, (list, tuple)) else imgsz
-        self.imgsz_h, self.imgsz_w = (
-            self.imgsz if isinstance(self.imgsz, tuple) else (self.imgsz, self.imgsz)
-        )
+        self.imgsz_h, self.imgsz_w = self.imgsz if isinstance(self.imgsz, tuple) else (self.imgsz, self.imgsz)
         self.border = (-self.imgsz_w // 2, -self.imgsz_h // 2)  # width, height
         self.n = n
         self.buffer_enabled = self.dataset.cache != "ram"
